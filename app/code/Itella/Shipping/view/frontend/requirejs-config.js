fdd1b79a99dae381f3a8f97a490966732780bdc7
Kestutis M <kestutis@mijora.lt>
2023-02-03 09:41:56 +0200

    Updated leaflet integration logic

--- app/code/Itella/Shipping/view/frontend/requirejs-config.js
+++ app/code/Itella/Shipping/view/frontend/requirejs-config.js
@@ -5,5 +5,17 @@ var config = {
                 'Itella_Shipping/js/action/set-shipping-information-mixin': true
             }
         }
+    },
+    paths: {
+        leaflet: 'https://unpkg.com/leaflet@1.6.0/dist/leaflet',
+        leafletmarkercluster: 'https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster'
+    },
+    shim: {
+        leaflet: {
+            exports: 'L'
+        },
+        leafletmarkercluster: {
+            deps: ['leaflet']
+        }
     }
 };
